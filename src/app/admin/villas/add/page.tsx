77133affb558275f9f6d0972aab9ee27ef3110d0
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-03 10:06:01 +0530

    admin model set up in hotels

---
+++ src/app/admin/villas/add/page.tsx
@@ -0,0 +1,12 @@
+import PageTitle from '<pages>/components/page-title'
+import React from 'react'
+
+function AddVilaPage() {
+  return (
+    <div>
+      <PageTitle title="Add Vila" />
+    </div>
+  )
+}
+
+export default AddVilaPage
