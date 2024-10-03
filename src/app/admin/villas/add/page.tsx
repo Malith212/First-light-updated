7b6dd8a9bb805ca1c452b3a36f820ef68a9ebb62
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-03 10:41:21 +0530

    hotel form added

--- src/app/admin/villas/add/page.tsx
+++ src/app/admin/villas/add/page.tsx
@@ -1,10 +1,12 @@
 import PageTitle from '<pages>/components/page-title'
 import React from 'react'
+import VillaForm from '../_common /villa-form'
 
 function AddVilaPage() {
   return (
     <div>
       <PageTitle title="Add Vila" />
+      <VillaForm />
     </div>
   )
 }
