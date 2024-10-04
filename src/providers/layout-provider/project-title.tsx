feb6db315e9b3c9708761a5e538b35fd753d998d
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-04 18:57:11 +0530

    loading spinner settings with add villa crud

--- src/providers/layout-provider/project-title.tsx
+++ src/providers/layout-provider/project-title.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/navigation";
 
-function ProjectTitle(){
+function ProjectTitle() {
+  const router = useRouter();
   return (
-    <div className="p-5 text-2xl font-bold border-0 border-r border-solid">First Light Villas</div>
-  )
+    <div
+      className="p-5 text-2xl font-bold border-0 border-r border-solid cursor-pointer"
+      onClick={() => router.push("/")}
+    >
+      First Light Villas
+    </div>
+  );
 }
- 
-export default ProjectTitle
+
+export default ProjectTitle;
