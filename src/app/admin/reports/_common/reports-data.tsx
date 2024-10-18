fdbba4459d351a9f23eefb165479039afb5d6db6
Malith21 <111115039+Malith21@users.noreply.github.com>
2024-10-18 19:54:08 +0530

    reports backend part

--- src/app/admin/reports/_common/reports-data.tsx
+++ src/app/admin/reports/_common/reports-data.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import BookingModel from "<pages>/models/booking-model";
+import React from "react";
+import AdminBookingsTable from "../../_common/admin-bookings-table";
 
-function ReportsData() {
+async function ReportsData({ searchParams }: { searchParams: any }) {
+  const response = await BookingModel.find({
+    bookingStatus: "Booked",
+    createdAt: {
+      $gte: searchParams.startDate,
+      $lte: searchParams.endDate,
+    },
+  }).populate("room").populate("user").populate("villa").sort({ createdAt: -1 });
+  const bookings = JSON.parse(JSON.stringify(response));
+  const totalBookings = bookings.length;
+  const totalRevenue = bookings.reduce(
+    (acc: number, booking: any) => acc + booking.totalAmount,
+    0
+  );
+
+
   return (
-    <div>ReportsData</div>
-  )
+    <div>
+      <div className="md:flex-row flex-col flex gap-10">
+        <div
+          className="border py-7 px-10 flex flex-col border-solid gap-5"
+          style={{
+            border: "1px solid #40679E",
+          }}
+        >
+          <h1 className="text-xl font-bold text-gray-600">Total Bookings</h1>
+          <h1
+            className="text-5xl font-bold text-center"
+            style={{ color: "#40679E" }}
+          >
+            {totalBookings}
+          </h1>
+        </div>
+
+        <div
+          className="border py-7 px-10 flex flex-col border-solid gap-5"
+          style={{
+            border: "1px solid #944E63",
+          }}
+        >
+          <h1 className="text-xl font-bold text-gray-600">Total Revenue</h1>
+          <h1
+            className="text-5xl font-bold text-center"
+            style={{ color: "#944E63" }}
+          >
+            LKR {totalRevenue}
+          </h1>
+        </div>
+      </div>
+
+      <AdminBookingsTable bookings={bookings} />
+    </div>
+  );
 }
 
-export default ReportsData
+export default ReportsData;
--- src/app/admin/reports/page.tsx
+++ src/app/admin/reports/page.tsx
@@ -1,18 +1,22 @@
-import PageTitle from '<pages>/components/page-title'
-import React from 'react'
-import ReportsFilters from './_common/reports-filters'
+import PageTitle from "<pages>/components/page-title";
+import React, { Suspense } from "react";
+import ReportsFilters from "./_common/reports-filters";
+import ReportsData from "./_common/reports-data";
+import Spinner from "<pages>/components/spinner";
 
-function ReportsPage({searchParams}: {searchParams: any}) {
-  console.log(searchParams);
+function ReportsPage({ searchParams }: { searchParams: any }) {
+  const suspenseKey = JSON.stringify(searchParams);
   return (
     <div>
       <PageTitle title="Reports" />
 
-      <ReportsFilters 
-      searchParams={searchParams}
-      />
+      <ReportsFilters searchParams={searchParams} />
+
+      <Suspense key={suspenseKey} fallback={<Spinner fullHeight />}>
+        <ReportsData searchParams={searchParams} />
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default ReportsPage
+export default ReportsPage;
